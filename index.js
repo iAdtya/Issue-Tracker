896ae7903e4d9fb333e3ac8fd155a58bbbd41c0b
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-11-25 22:22:13 +0530

    push

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import routes from "./routes/index.js";
@@ -9,23 +9,34 @@ const app = express();
 const port = process.env.port || 8001;
 console.log(port);
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(express.json())
 
-app.set('view engine','ejs');
-app.set('views','views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 app.use("/", routes);
 
-await connectDB();
-app.listen(port, async (error)=>{
-  if(error){
-    console.log(`error in running the server :: ${error}`);
-    return;
+// await connectDB();
+// app.listen(port, async (error)=>{
+//   if(error){
+//     console.log(`error in running the server :: ${error}`);
+//     return;
+//   }
+//   console.log(`server is running on port :: ${port}`)
+// })
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, async () => {
+      console.log(`server is running on port :: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
   }
-  console.log(`server is running on port :: ${port}`)
-})
+};
 
-
+startServer();
