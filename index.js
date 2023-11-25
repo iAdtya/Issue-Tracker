80e5a0089e59b2941a9c7c49f2c3e40a6685b8df
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-11-25 22:12:47 +0530

    push

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import { connectDB } from "./config/mongoose.js";
 
 const app = express();
 
-const port = process.env.port;
+const port = process.env.port || 8001;
+console.log(port);
 
 app.use(express.urlencoded({extended:true}));
 // app.use(express.json())
@@ -18,13 +19,13 @@ app.use(express.static('./assets'));
 
 app.use("/", routes);
 
+await connectDB();
 app.listen(port, async (error)=>{
   if(error){
     console.log(`error in running the server :: ${error}`);
     return;
   }
   console.log(`server is running on port :: ${port}`)
-  await connectDB();
 })
 
 
